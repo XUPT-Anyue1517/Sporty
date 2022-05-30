5219f19f34f25651a729261b7663a7a1e6889e05
2193560021 <62364555+2193560021@users.noreply.github.com>
2022-05-30 21:40:40 +0800

    上传用用户端大体结构

---
+++ src/main/resources/vue/src/views/CarSearch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    汽车查询
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarSearch"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/resources/vue/src/views/Index.vue
+++ src/main/resources/vue/src/views/Index.vue
@@ -1,5 +1,16 @@
 <template>
+  <div>
+    <div class="block text-center">
+      <el-carousel height="650px">
+        <el-carousel-item v-for="item in car_img" >
+          <img :src="item" class="small justify-center" style="width: 100%"/>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
 
+
+
+  </div>
 </template>
 
 <script>
@@ -7,7 +18,13 @@ export default {
   name: "Index",
   data(){
     return{
-      path:this.$route.path
+      path:this.$route.path,
+      car_img:[
+        require("../assets/img/car/car_img/wallhaven-n63966_1920x1080.png"),
+        require("../assets/img/car/car_img/wallhaven-4gg7l7_1920x1080.png"),
+        require("../assets/img/car/car_img/wallhaven-ymzzdl_1920x1080.png"),
+        require("../assets/img/car/car_img/wallhaven-43kdr6.jpg")
+      ]
     }
   },
   created() {
